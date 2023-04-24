3a2f8ec4573247921ea37646631b28bfa2fd649e
Soo <soojlee0106@naver.com>
2023-04-24 18:41:27 +0900

    try 2 clusters

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import seaborn as sns
+import PIL
 
 # Read the data
 data = pd.read_csv('crypto-data.csv')
@@ -37,9 +40,12 @@ data['% 1h'] = (data['% 1h'] - data['% 1h'].mean()) / data['% 1h'].std()
 data['% 24h'] = (data['% 24h'] - data['% 24h'].mean()) / data['% 24h'].std()
 data['% 7d'] = (data['% 7d'] - data['% 7d'].mean()) / data['% 7d'].std()
 
+# remove outliers
+data = data[(np.abs(data['Market Cap']) < 3) & (np.abs(data['Volume(24h)']) < 3) & (np.abs(data['Price']) < 3) & (np.abs(data['% 1h']) < 3) & (np.abs(data['% 24h']) < 3) & (np.abs(data['% 7d']) < 3)]
+
 # cluster the data using k-means clustering
-kmeans = KMeans(n_clusters=5)
-kmeans.fit(data.drop(['Rank', 'Name', 'Platform', 'Circulating Supply'], axis=1))
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(data.drop(['Rank', 'Name', 'Platform', 'Circulating Supply', '% 1h', '% 24h', '% 7d'], axis=1))
 
 # create a new column for the cluster
 data['Cluster'] = kmeans.labels_ 
@@ -47,3 +53,14 @@ data['Cluster'] = kmeans.labels_
 # save the data
 data.to_csv('crypto-data-clustered.csv', index=False)
 
+# show the data with the clusters 
+plt.figure(figsize=(10, 6))
+plt.scatter(data['Market Cap'], data['Price'], c=data['Cluster'], cmap='rainbow')
+plt.xlabel('Market Cap')
+plt.ylabel('Price')
+
+# save the plot as jpg
+plt.savefig('crypto-data-clustered.png')
+
+plt.show()
+plt.close()
