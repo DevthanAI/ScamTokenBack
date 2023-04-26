de2a677e14ca10b2e3371cba5f34ad122243abcc
Soo <soojlee0106@naver.com>
2023-04-26 11:23:21 +0900

    add explanation in readme

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
+
+## Explanation
+
+This project uses clustering to detect whether a token is a scam or not. The idea is that if a token is a scam, it will have a similar transaction history to other scam tokens. This is because the scam tokens are likely to be created by the same people, and therefore will have similar transaction histories.
+
+Initally, K-means and HDBSCAN were used to cluster the tokens. HDBSCAN did not perform well, likely because not all the dimensions were relevant. K-means performed better after PCA was used to reduce the dimensionality of the data to 2.
+
+Afterwards, using the completed K-means model, clustering was applied to test how it would analyze tokens that had "OK" reputation (according to Etherscan), and tokens that had suspicious transaction histories. The results are as follows:
+
+The tokens that had "OK" reputation were all in cluster 1,2,4.
+The tokens that had suspicious transaction histories were mostly in 0,2,3.
+
+Therefore, it is safe to say that if a token is found in cluster 1 and 4, it is likely to be safe.
+On the other hand, if it is found in cluster 0 and 3, it is likely to be a scam.
+For cluster 2, it is hard to tell, as it contains both safe and scam tokens, but it is likely to be a scam so buyers should be aware.
---
+++ sus-cluster.py
@@ -0,0 +1,64 @@
+import pickle
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+# open the pickled model
+kmeans = pickle.load(open('pickle/k-means.pkl', 'rb'))
+
+# open the dataset
+data = pd.read_csv('csv/sus-label.csv')
+
+# remove $ from Market Cap, Volume and Price columns
+data['Market Cap'] = data['Market Cap'].str.replace('$', '')
+data['Volume(24h)'] = data['Volume(24h)'].str.replace('$', '')
+data['Price'] = data['Price'].str.replace('$', '')
+
+# remove commas from Market Cap, Volume and Price columns
+data['Market Cap'] = data['Market Cap'].str.replace(',', '')
+data['Volume(24h)'] = data['Volume(24h)'].str.replace(',', '')
+data['Price'] = data['Price'].str.replace(',', '')
+
+# remove % from % 1h, % 24h, and % 7d columns
+data['% 1h'] = data['% 1h'].str.replace('%', '')
+data['% 24h'] = data['% 24h'].str.replace('%', '')
+data['% 7d'] = data['% 7d'].str.replace('%', '')
+
+# remove row with ? in Volume(24h) column
+data = data[data['Volume(24h)'] != '?']
+
+# remove row with excessively small values in price column
+data = data[data['Price'] != '0.0...04719']
+data = data[data['Price'] != '0.0...07364']
+
+# convert data to numeric
+data = data.apply(pd.to_numeric, errors='ignore')
+
+# standardize the data using standard scaler
+scaler = StandardScaler()
+scaler.fit(data.drop(['Rank', 'Name', 'Platform', 'Circulating Supply', 'reputation'], axis=1))
+scaled_features = scaler.transform(data. drop(['Rank', 'Name', 'Platform', 'Circulating Supply', 'reputation'], axis=1))
+
+# create a new dataframe with the scaled features
+data = pd.DataFrame(scaled_features, columns=['Market Cap', 'Volume(24h)', 'Price', '% 1h', '% 24h', '% 7d'])
+
+# apply PCA to reduce the dimensions of the data
+pca = PCA(n_components=2)
+pca.fit(data)
+x_pca = pca.transform(data)
+
+# create a new dataframe with the PCA features
+data = pd.DataFrame(x_pca, columns=['Market Cap', 'Price'])
+
+# predict the clusters
+y_kmeans = kmeans.predict(x_pca)
+
+# add the clusters to the dataset
+data['Cluster'] = y_kmeans
+
+# calculate the percentage of tokens in each cluster
+print(data['Cluster'].value_counts(normalize=True))
+
+# save the dataset
+data.to_csv('csv/sus-cluster.csv', index=False)
+
